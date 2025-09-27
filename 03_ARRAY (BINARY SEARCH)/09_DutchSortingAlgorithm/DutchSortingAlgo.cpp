416fb1b0af7e02f6eeeae52fb7871b28e6dd4c95
Vaibhav Belekar <vaibhavsbelekar7@gmail.com>
2025-09-28 01:27:32 +0530

    learn about dutch national flag algoritham

---
+++ 03_ARRAY (BINARY SEARCH)/09_DutchSortingAlgorithm/DutchSortingAlgo.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+using namespace std ;
+
+void sortColors(vector<int>& nums) {
+        int n=nums.size();
+        int mid=0,low=0,high=n-1;
+
+        while(mid<=high){
+            if(nums[mid]==0){
+              swap(nums[low],nums[mid]);
+              mid++;
+              low++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+
+            }else{
+                swap(nums[mid],nums[high]);
+                high--;
+            }
+        
+        }
+        
+    }
+
+int main(){
+    vector<int> nums={1,2,1,1,0,0,2,1};
+    sortColors(nums);
+
+    // print sorted array
+    for(int x : nums){
+        cout << x << " ";
+    }
+    cout << endl;
+    return 0;
+}
